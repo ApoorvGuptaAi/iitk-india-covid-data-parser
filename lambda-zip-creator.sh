7c018003d1f78877ab46a19ebc8ea490f704bb3e
Apoorv Gupta <apoorv@ailens.io>
2021-05-02 21:50:59 -0700

    Lambda creator.

---
+++ lambda-zip-creator.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+# Based on https://docs.aws.amazon.com/pinpoint/latest/developerguide/tutorials-importing-data-create-python-package.html
+# and https://towardsdatascience.com/serverless-covid-19-data-scraper-with-python-and-aws-lambda-d6789a551b78
+
+
+PYTHON_BINARY=python3.7  # Change for your machine, ensure this refers to a python3.x binary.
+
+$PYTHON_BINARY -m pip install requests beautifulsoup4 -t upload/
+cp *.py upload/
+pushd upload/
+zip -r ../lambda.zip requests bs4 chardet lambda_function.py certifi idna backports urllib3 soupsieve *.py
+popd
