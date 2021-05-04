f881ff85f74525be448c230fd2d38227e5207c76
Sriraj Badam <srirajdutt@google.com>
2021-05-04 16:09:14 -0700

    Add Uttarakhand parser

--- lambda_function.py
+++ lambda_function.py
@@ -9,6 +9,7 @@ from noida_up_parser import get_noida_hospitals
 from ranchi_parser import get_ranchi_hospitals
 from haryana_parser import get_haryana_hospitals
 from bihar_parser import get_bihar_hospitals
+from uttarakhand_parser import get_uttarakhand_hospitals
 
 _VERSION = 1
 
@@ -41,6 +42,8 @@ def main(request):
         url_hospitals_map = get_haryana_hospitals()
     elif state_filter == "Bihar":
         url_hospitals_map = get_bihar_hospitals()
+    elif state_filter == "Uttarakhand":
+        url_hospitals_map = get_uttarakhand_hospitals()
     else:
         url_hospitals_map = generic_hospital_get_data(state_filter=state_filter,
                                                       city_filter=city_filter)
@@ -79,4 +82,4 @@ def lambda_handler(event, context):
 
 if __name__ == "__main__":
     #print(main({'state': 'Jharkhand', 'city': 'Ranchi'}))
-    main({'state': 'Gujarat'})
+    print(main({'state': 'Uttarakhand'}))
---
+++ uttarakhand_parser.py
@@ -0,0 +1,62 @@
+import requests
+import dateutil.parser
+from bs4 import BeautifulSoup
+
+from hospital import Hospital, Resource, ResourceType
+from database_helper import upload_hospitals
+
+from dateutil import parser
+from dateutil import tz
+
+tzinfos = {"IST": tz.gettz('Asia/Kolkata')}
+
+# List of web data sources
+UTTARAKHAND_URL = 'https://covid19.uk.gov.in/bedssummary.aspx'
+
+def parse_hospital_row(row_data):
+  district = row_data.find('span', {'id' : 'lblDistrictName'}).text
+  hospitalName = row_data.find('span', {'id' : 'lblhospitalName'}).text
+  availableGenBeds = int(row_data.find('span', {'id' : 'Lbloccupiedgenralbeds'}).text)
+  totalGenBeds = int(row_data.find('span', {'id' : 'lbltotGenralbeds'}).text)
+  availableOxyBeds = int(row_data.find('span', {'id' : 'lbloccupiedoxygenbeds'}).text)
+  totalOxyBeds = int(row_data.find('span', {'id' : 'lbltotoxygenbeds'}).text)
+  availableICUBeds = int(row_data.find('span', {'id' : 'lbloccupiedicubeds'}).text)
+  totalICUBeds = int(row_data.find('span', {'id' : 'lbltoticubeds'}).text)
+  lastUpdatedAt = parser.parse(row_data.find('span', {'id' : 'lbllastupdated'}).text + "IST", tzinfos=tzinfos)
+  resources = [
+               Resource(ResourceType.BED_WITHOUT_OXYGEN, "", availableGenBeds, totalGenBeds),
+               Resource(ResourceType.BED_WITH_OXYGEN, "", availableOxyBeds, totalOxyBeds),
+               Resource(ResourceType.ICUS, "", availableICUBeds, totalICUBeds),
+  ]
+  hospital = Hospital(
+      hospitalName, "", district, "", "Uttarakhand", "", lastUpdatedAt,
+      resources, "", UTTARAKHAND_URL, 0)
+  return hospital
+
+def get_data_from_web():
+  webpage = requests.get(UTTARAKHAND_URL)
+  return BeautifulSoup(webpage.text, 'html.parser')
+
+def parse_web_data(soup):
+  table = soup.find('table', {'id': 'grdhospitalbeds'})
+  hospital_rows = table.find('tbody').find_all('tr')
+  hospitals = []
+  for row_data in hospital_rows:
+    hospital = parse_hospital_row(row_data)
+    hospitals.append(hospital)
+  return hospitals
+
+def get_uttarakhand_hospitals():
+  scraped_data = get_data_from_web()
+  hospitals = parse_web_data(scraped_data)
+  return { UTTARAKHAND_URL: hospitals }
+
+
+def main():
+  hospital_data = get_uttarakhand_hospitals()
+  print(len(hospital_data[UTTARAKHAND_URL]))
+  #upload_hospitals(hospital_data)
+
+
+if __name__ == "__main__":
+    main()
