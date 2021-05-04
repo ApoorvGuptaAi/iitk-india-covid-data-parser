cd7d886f96925435b1865334d678413429cfd105
Apoorv Gupta <apoorv@ailens.io>
2021-05-04 09:31:26 -0700

    Update printing.

--- lambda_function.py
+++ lambda_function.py
@@ -1,14 +1,29 @@
+from typing import List
 import json
 import time
 
+from hospital import Resource, Hospital
+from database_helper import upload_hospitals, resource_to_json
 from generic_hospital_parser import get_data as generic_hospital_get_data
 from noida_up_parser import get_noida_hospitals
 from ranchi_parser import get_ranchi_hospitals
-from database_helper import upload_hospitals
 from haryana_parser import get_haryana_hospitals
 from bihar_parser import get_bihar_hospitals
 
 _VERSION = 1
+
+
+def summarize_resources(hospitals: List[Hospital]):
+    resources = {}
+    for hospital in hospitals:
+        for resource in hospital.resources:
+            r_type = resource.resource_type
+            if r_type not in resources:
+                resources[r_type] = Resource(r_type, '', 0, 0)
+            summary = resources[r_type]
+            summary.total_qty += resource.total_qty
+            summary.resource_qty += resource.resource_qty
+    return {k: resource_to_json(v) for (k, v) in resources.items()}
 
 
 def main(request):
@@ -35,19 +50,21 @@ def main(request):
     outputs = []
     for url in url_hospitals_map:
         hospitals = url_hospitals_map[url]
+        resources_summary = summarize_resources(hospitals)
         start = time.time()
         size = len(hospitals)
-        upload_hospitals(hospitals, job_id)
+        # upload_hospitals(hospitals, job_id)
         output = {
             "state": state_filter,
             "url": url,
             "size": size,
-            "duration": time.time() - start
+            "duration": time.time() - start,
+            "resources": list(resources_summary.values())
         }
         if city_filter:
             output["city"] = city_filter
         outputs.append(output)
-    output_str = json.dumps({"outputs": outputs})
+    output_str = json.dumps({"outputs": outputs}, indent=2)
     print(output_str)
     return output_str
 
