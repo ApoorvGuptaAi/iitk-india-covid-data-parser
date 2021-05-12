6d30c488b104874ccadb507ad8bbae68a0075a6d
Sunny Bhagavathula <sunny@orbitaleffects.us>
2021-05-11 22:35:28 -0400

    Adding Lucknow Parser and fixing some issues with Panvel

---
+++ lucknow_parser.py
@@ -0,0 +1,80 @@
+import requests
+import logging
+from bs4 import BeautifulSoup
+from dateutil import parser
+from hospital import Hospital, Resource, ResourceType
+from generic_html_parser import HtmlHospitalParser
+
+
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO)
+
+
+class LucknowParser(HtmlHospitalParser):
+    URL = "https://beds.dgmhup-covid19.in/EN/covid19bedtrack"
+    hospital_city = 'Lucknow'
+    hospital_state = 'Uttar Pradesh'
+
+    def __init__(self):
+        self.page_soup = None
+        self.hospitals = []
+
+    def read_page(self):
+        response = requests.get(self.URL)
+        self.page_soup = BeautifulSoup(response.text, 'html.parser')
+        return self
+
+    def parse_hospitals(self):
+        hospital_containers = self.page_soup.find('table').find_all('tr')
+        container_idx = 0
+        full_table = self.page_soup.find_all('table')[3]
+        hospital_keys = [x.get_text() for x in full_table.find_all('span')]
+        hospital_values = [x.get_text() for x in full_table.find_all('a', {'class': 'style98'})]
+        hospital_keys = [tuple(hospital_keys[i:i + 5]) for i in range(0, len(hospital_keys), 5)]
+        hospital_values = [tuple(hospital_values[i:i + 2]) for i in range(0, len(hospital_values), 2)]
+        assert len(hospital_keys) == len(hospital_values), "Could not match up the hospital keys with hospital values " \
+                                                           "- may be a change in schema"
+        num_hospitals = len(hospital_keys)
+        logger.info(f'Picked up data for {num_hospitals} hospitals in {self.hospital_city}')
+        hospital_idx = 0
+        for key_tupl, val_tupl in zip(hospital_keys, hospital_values):
+            try:
+                resources = [Resource(ResourceType.BEDS, '', int(val_tupl[1]), int(val_tupl[0]))]
+                # TODO: This is a dynamic site that will provide more details on ICU/OXYGEN on click...figure that out
+                try:
+                    update_date = parser.parse(key_tupl[2], dayfirst=True)
+                    address = key_tupl[3]
+                except parser.ParserError:
+                    update_date = parser.parse(key_tupl[3], dayfirst=True)
+                    address = key_tupl[2]
+                hospital = Hospital(**{'name': key_tupl[0],
+                                       'resources': resources,
+                                       'address': address.strip(),
+                                       'city': self.hospital_city,
+                                       'url': self.URL,
+                                       'state': self.hospital_state,
+                                       'last_updated': update_date})
+                self.hospitals.append(hospital)
+                logger.info(f'Parsed hospital: {hospital.name} with {len(hospital.resources)} resources')
+            except Exception as e:
+                logger.warning(f'Cannot parse hospital idx {hospital_idx}: {e}')
+        if not self.hospitals:
+            raise Exception(f'Could not pick up any hospitals - schema likely changed for {self.URL}')
+        logger.info(f'Picked up a total of {len(self.hospitals)} hospitals in '
+                    f'{self.hospital_city}')
+        return self
+
+    @staticmethod
+    def export_hospital_data():
+        lucknow_parser = LucknowParser().read_page().parse_hospitals()
+        return {lucknow_parser.URL: lucknow_parser.hospitals}
+
+
+def get_hospital_data():
+    return LucknowParser.export_hospital_data()
+
+
+# Uncomment for test
+# if __name__ == '__main__':
+#     dat = get_hospital_data()
+#     logger.info(f'Test complete')
--- maharashtra_panvel_parser.py
+++ maharashtra_panvel_parser.py
@@ -1,7 +1,7 @@
 import requests
 import logging
 from bs4 import BeautifulSoup
-from datetime import datetime
+from dateutil import parser
 from hospital import Hospital, Resource, ResourceType
 from generic_html_parser import HtmlHospitalParser
 
@@ -44,30 +44,27 @@ class PanvelParser(HtmlHospitalParser):
         name_elem = container.find('h4')
         if not name_elem:
             raise Exception(f'Cannot read name of hospital')
-        resource_types = [
-            ResourceType.ICUS, ResourceType.BED_WITH_OXYGEN,
-            ResourceType.BED_WITHOUT_OXYGEN, ResourceType.ICU_WITH_VENTILATOR
-        ]
         resource_mapper = {'ICU Vacant': ResourceType.ICUS,
-                           'Non ICU Vacant': ResourceType.BED_WITHOUT_OXYGEN,
+                           'Non ICU Vacant': ResourceType.BEDS,
                            'Ventilator Available': ResourceType.ICU_WITH_VENTILATOR}
-        catagories = [x.get_text() for x in container.find_all("div", {'class': 'text-white mb-0'})]
+        categories = [x.get_text() for x in container.find_all("div", {'class': 'text-white mb-0'})]
         numbers = [int(x.find_all('b')[0].get_text()) for x in container.find_all("div", {'class': 'h1 m-0'})]
         hospital_name = name_elem.get_text()
-        assert catagories == ['Capacity', 'Occupied', 'Vacant',
-                              'ICU Vacant', 'Non ICU Vacant', 'Ventilator Available'], f"Cant read catagories"
-        assert len(catagories) == len(numbers), f"Unable to match up numbers and categories for {hospital_name} details"
-        resource_dict = {k: v for k, v in zip(catagories, numbers)}
+        assert categories == ['Capacity', 'Occupied', 'Vacant',
+                              'ICU Vacant', 'Non ICU Vacant', 'Ventilator Available'], f"Cant read categories"
+        assert len(categories) == len(numbers), f"Unable to match up numbers and categories for {hospital_name} details"
+        resource_dict = {k: v for k, v in zip(categories, numbers)}
         resources = []
         for resource_name, resource_type in resource_mapper.items():
             resources.append(Resource(resource_type, '', resource_dict[resource_name]))
+        time_stamp = container.find('span', {'class': 'pull-right'}).get_text().split('Updated :')[-1]
         hospital = Hospital(**{'name': hospital_name,
                                'resources': resources,
                                'district': self.hospital_district,
                                'city': self.hospital_city,
                                'url': self.URL,
                                'state': self.hospital_state,
-                               'last_updated': datetime.utcnow(),
+                               'last_updated': parser.parse(time_stamp, dayfirst=True),
                                'debug_text': str(container)})
         self.hospitals.append(hospital)
         logger.info(f'Parsed hospital: {hospital.name} with {len(hospital.resources)} resources')
@@ -82,5 +79,7 @@ def get_hospital_data():
     return PanvelParser.export_hospital_data()
 
 
+# Uncomment for test
 # if __name__ == '__main__':
-#     get_hospital_data()
+#     dat = get_hospital_data()
+#     logger.info(f'Test complete')
